363791f0f5625752ff311309b1e5aff971c04bb1
TheUnknown1221 <80987328+TheUnknown1221@users.noreply.github.com>
2021-08-23 01:52:15 +0000

    Update user.h

--- multi files with classes/user.h
+++ multi files with classes/user.h
@@ -13,8 +13,8 @@ class User
         static int getUserCount();
 
     User(std::string firstName, std::string lastName, int age);
-
     ~User();
+    
     friend std::istream& operator >> (std::istream& input, User &user);
     friend std::ostream& operator << (std::ostream& output, User user);
 };
